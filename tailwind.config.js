cc9d20d2c3a119db72d5e4679452efef31470475
Shardool Singh <shardoolsingh4@gmail.com>
2025-07-03 23:19:25 +0530

    added tailwindcss to the codebase

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js,ts,jsx,tsx}", // Adjust based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
